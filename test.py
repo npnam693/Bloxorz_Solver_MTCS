991c08db861f5f076f25f16f43b7f750fc9c28ac
TruongNguyen1305 <trngcr71305@gmail.com>
2023-03-23 22:20:29 +0700

    nghich

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from read_level_input import read_file
 from block import Block
 from BFS_algorithm import BFS
 import time
-
+import tracemalloc
 
 def test():
     cnt_success = 0
@@ -15,14 +15,18 @@ def test():
         block = Block(game_setup.start_x, game_setup.start_y, "STAND", None, game_map)
         start_time = time.time()
         print("Testing level", level, " ..........")
+        tracemalloc.start()
         solution = BFS(block)
+        memory_used, peak_memory = tracemalloc.get_traced_memory()
         if solution:
             cnt_success += 1
             # with open('test_result.txt', 'a') as f:
             #     print('success\n''Level ', str(level), ":     \
             #     Found solution in    ", round(time.time() - start_time, 9), '("s")', file=f)
             print("Level ", str(level), ":     Found solution in    ", round(time.time() - start_time, 9), ("s"))
+            print(f"Memory used: {memory_used} Bytes")
         else:
             print("Failed to find solution :( ")
         print("===========================================")
+        tracemalloc.stop()
     print(cnt_success ,"/33 level success")
